32760608c70863813739c8f9ddf213615d337004
Hugo <hugo.lacombe@epfedu.fr>
2022-02-21 12:16:57 +0100

    création menu principal

--- app/src/main/java/com/example/applicationsiah/Menu_principal.java
+++ app/src/main/java/com/example/applicationsiah/Menu_principal.java
@@ -3,13 +3,11 @@ package com.example.applicationsiah;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Menu_principal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        /* eeeee */
+        setContentView(R.layout.activity_menu_principal);
     }
 }
