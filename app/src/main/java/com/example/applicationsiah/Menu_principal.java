80f916dd175ecba1a3a1169f7277bb48bbeb9e39
scottfullagar <scott.fullagar@epfedu.fr>
2022-02-22 11:24:05 +0100

    debut creation menu deroulant dans menu_principal

--- app/src/main/java/com/example/applicationsiah/Menu_principal.java
+++ app/src/main/java/com/example/applicationsiah/Menu_principal.java
@@ -2,6 +2,8 @@ package com.example.applicationsiah;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Menu_principal extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class Menu_principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        // 1) creation d’un objet-reference spinner vers le spinner de l’activite
+        Spinner monSpinner = (Spinner) findViewById(R.id.menu_sports);
+        // 2) creation d’un ArrayAdapter à partir de tab_pokemon et mise en forme par defaut
+        ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource (this,
+                R.array.menu_sports, android.R.layout.simple_spinner_item);
+        // 3) définition de la mise en page
+        monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // 4) Association de adapter au spinner
+        monSpinner.setAdapter(monAdapter);
     }
 }
