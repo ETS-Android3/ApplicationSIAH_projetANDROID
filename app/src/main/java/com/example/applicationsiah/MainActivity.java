726c165ac75ddd5f6a90d35736943fa0808df7c6
ilonabematol <ilona.bematol@epfedu.fr>
2022-02-22 11:11:59 +0100

    résolution pb : page d'acceuil puis passage au menu principal OK!

--- app/src/main/java/com/example/applicationsiah/MainActivity.java
+++ app/src/main/java/com/example/applicationsiah/MainActivity.java
@@ -8,21 +8,29 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button monBouton = (Button)findViewById(R.id.button_demarrer);
         monBouton.setText("Validez");
-        monBouton.setOnClickListener(new View.OnClickListener());
+        monBouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                visualiser_la_suite();
+            }
+        });
 
     }
-    private void visualiser_la_suite()
-    {
+    private void visualiser_la_suite() {
         Intent intent = new Intent(this, Menu_principal.class);
         startActivity(intent);
-        public void onClick(View view) {
-        visualiser_la_suite();
     }
-    }
+       // public void onClickListener(View view) {
+
+    //}
+
 }
