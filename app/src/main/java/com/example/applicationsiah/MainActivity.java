528dc7dfaeb12067635805779a82f01adad1d031
ilonabematol <ilona.bematol@epfedu.fr>
2022-02-22 10:35:17 +0100

    avancée du bouton démarrer

--- app/src/main/java/com/example/applicationsiah/MainActivity.java
+++ app/src/main/java/com/example/applicationsiah/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.applicationsiah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button monBouton = (Button)findViewById(R.id.button_demarrer);
+        monBouton.setText("Validez");
+        monBouton.setOnClickListener(new View.OnClickListener());
 
-
+    }
+    private void visualiser_la_suite()
+    {
+        Intent intent = new Intent(this, Menu_principal.class);
+        startActivity(intent);
+        public void onClick(View view) {
+        visualiser_la_suite();
+    }
     }
 }
