10bd2387437e63e8328327ade94d565916aa7f58
scottfullagar <scott.fullagar@epfedu.fr>
2022-02-22 11:45:48 +0100

    Menu deroulant fonctionel dans le menu principal

--- app/src/main/java/com/example/applicationsiah/Menu_principal.java
+++ app/src/main/java/com/example/applicationsiah/Menu_principal.java
@@ -15,7 +15,7 @@ public class Menu_principal extends AppCompatActivity {
         Spinner monSpinner = (Spinner) findViewById(R.id.menu_sports);
         // 2) creation d’un ArrayAdapter à partir de tab_pokemon et mise en forme par defaut
         ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource (this,
-                R.array.menu_sports, android.R.layout.simple_spinner_item);
+                R.array.tab_sports, android.R.layout.simple_spinner_item);
         // 3) définition de la mise en page
         monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // 4) Association de adapter au spinner
