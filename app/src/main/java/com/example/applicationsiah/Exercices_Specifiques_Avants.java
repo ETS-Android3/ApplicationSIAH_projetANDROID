a3d599f530bd0873e5922afe5b5132eaa59bcf30
scottfullagar <scott.fullagar@epfedu.fr>
2022-02-28 23:42:44 +0100

    creation de l'interface des conseils sportifs et des exos specifique (rugby)

---
+++ app/src/main/java/com/example/applicationsiah/Exercices_Specifiques_Avants.java
@@ -0,0 +1,13 @@
+package com.example.applicationsiah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercices_Specifiques_Avants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercices_specifiques_avants);
+    }
+}
