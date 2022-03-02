a9f338857d9b49f3c5056653caa7b92065c2f077
Axel <axel.fourcadecancelle@epfedu.fr>
2022-03-02 01:39:29 +0100

    menu pour chaque poste de foot

---
+++ app/src/main/java/com/example/applicationsiah/menu_foot_bu.java
@@ -0,0 +1,13 @@
+package com.example.applicationsiah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menu_foot_bu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_foot_bu);
+    }
+}
---
+++ app/src/main/java/com/example/applicationsiah/menu_foot_dg_dd.java
@@ -0,0 +1,13 @@
+package com.example.applicationsiah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menu_foot_dg_dd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_foot_dg_dd);
+    }
+}
