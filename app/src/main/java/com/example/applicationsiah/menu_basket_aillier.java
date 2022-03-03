0f58dbb6b30e7aa305d72070399a5dfa2db656e7
ilonabematol <ilona.bematol@epfedu.fr>
2022-03-03 23:03:09 +0100

    comm hugoooo

--- app/src/main/java/com/example/applicationsiah/menu_basket_aillier.java
+++ app/src/main/java/com/example/applicationsiah/menu_basket_aillier.java
@@ -9,5 +9,6 @@ public class menu_basket_aillier extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_basket_aillier);
+        // coucou hugo
     }
 }
