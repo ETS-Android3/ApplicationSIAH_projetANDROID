88d7f9c37c75f4c367551fb2e9fa4827c012d2e9
scottfullagar <scott.fullagar@epfedu.fr>
2022-03-03 10:25:47 +0100

    ajout bouton avant (passage de menu rugby a interface sportif)

--- app/src/main/java/com/example/applicationsiah/menu_rugby.java
+++ app/src/main/java/com/example/applicationsiah/menu_rugby.java
@@ -1,7 +1,10 @@
 package com.example.applicationsiah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menu_rugby extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class menu_rugby extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_rugby);
+        Button button_avant = (Button) findViewById(R.id.button_avant);
+        button_avant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                visualiser_interface_conseil_sportif();
+            }
+
+
+        });
+
     }
+
+    private void visualiser_interface_conseil_sportif() {
+        Intent intent = new Intent(this,interface_conseil_sportif.class);
+        startActivity(intent);
+    }
+
 }
