ef544093fd71ed4697814f510c02267b8bec7bd2
scottfullagar <scott.fullagar@epfedu.fr>
2022-03-04 00:02:01 +0100

    finalisation bouton arrieres rugby

--- app/src/main/java/com/example/applicationsiah/menu_rugby.java
+++ app/src/main/java/com/example/applicationsiah/menu_rugby.java
@@ -12,11 +12,73 @@ public class menu_rugby extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_rugby);
-        Button button_avant = (Button) findViewById(R.id.button_avant);
-        button_avant.setOnClickListener(new View.OnClickListener() {
+        Button button_9 = (Button) findViewById(R.id.button_10);
+        button_9.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                visualiser_interface_conseil_sportif();
+                visualiser_exercices_specifiques_arrieres();
+            }
+
+
+        });
+
+        Button button_dix = (Button) findViewById(R.id.button_dix);
+        button_dix.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                visualiser_exercices_specifiques_arrieres();
+            }
+
+
+        });
+        Button button_11 = (Button) findViewById(R.id.button_11);
+        button_11.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                visualiser_exercices_specifiques_arrieres();
+            }
+
+
+        });
+        Button button_12 = (Button) findViewById(R.id.button_12);
+        button_12.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                visualiser_exercices_specifiques_arrieres();
+            }
+
+
+        });
+        Button button_13 = (Button) findViewById(R.id.button_13);
+        button_13.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                visualiser_exercices_specifiques_arrieres();
+            }
+
+
+        });
+        Button button_14 = (Button) findViewById(R.id.button_14);
+        button_14.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                visualiser_exercices_specifiques_arrieres();
+            }
+
+
+        });
+        Button button_15 = (Button) findViewById(R.id.button_15);
+        button_15.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                visualiser_exercices_specifiques_arrieres();
             }
 
 
@@ -24,8 +86,8 @@ public class menu_rugby extends AppCompatActivity {
 
     }
 
-    private void visualiser_interface_conseil_sportif() {
-        Intent intent = new Intent(this,interface_conseil_sportif.class);
+    private void visualiser_exercices_specifiques_arrieres() {
+        Intent intent = new Intent(this,exercices_specifiques_arrieres.class);
         startActivity(intent); //test
     }
 
