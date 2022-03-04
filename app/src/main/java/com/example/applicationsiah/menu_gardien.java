9846d335ae8afe035703b01bf7250f272b99fc8c
Hugo <hugo.lacombe@epfedu.fr>
2022-03-04 01:04:28 +0100

    menu gardien fait, menu tacquetiqueau gb en cours

--- app/src/main/java/com/example/applicationsiah/menu_gardien.java
+++ app/src/main/java/com/example/applicationsiah/menu_gardien.java
@@ -1,7 +1,10 @@
 package com.example.applicationsiah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menu_gardien extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class menu_gardien extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_gardien);
+        Button button_tactique_gb = (Button) findViewById((R.id.button_tact_gb));
+        button_tactique_gb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                visualiser_la_tactiquegb();
+            }
+        });
+
+    }
+
+    private void visualiser_la_tactiquegb() {
+        Intent intent1 = new Intent(this, menu_tactique_gb.class);
+        startActivity(intent1);
     }
 }
