7cb837eca090fd065dbbbf91b986031cc7605ff9
ilonabematol <ilona.bematol@epfedu.fr>
2022-03-18 10:16:59 +0100

    couleur du menu pour le basket

--- app/src/main/java/com/example/applicationsiah/menu_basket_aillier.java
+++ app/src/main/java/com/example/applicationsiah/menu_basket_aillier.java
@@ -10,5 +10,6 @@ public class menu_basket_aillier extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_basket_aillier);
         // coucou hugo
+
     }
 }
