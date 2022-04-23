d76dca84387b0ea6615350cb6374810ffab0cd96
Hugo <hugo.lacombe@epfedu.fr>
2022-04-23 17:10:25 +0200

    j'ai modif podo

--- app/src/main/java/com/example/applicationsiah/Podometre.java
+++ app/src/main/java/com/example/applicationsiah/Podometre.java
@@ -14,44 +14,66 @@ public class Podometre extends AppCompatActivity implements SensorEventListener 
 SensorManager sensorManager;
 TextView tv_steps;
 boolean running = false;
+Sensor podometre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podometre);
-        System.out.print("Ici1");
+
         tv_steps=(TextView)  findViewById(R.id.tv_steps);
         sensorManager = (SensorManager)  getSystemService(Context.SENSOR_SERVICE);
+        podometre = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(podometre.isWakeUpSensor()==false){
+            tv_steps.setText("pute");
+        };
     }
 
     @Override
     protected void onResume() {
-        super.onResume();
+        sensorManager.registerListener(this,podometre,SensorManager.SENSOR_DELAY_UI);
+        if(podometre.isWakeUpSensor()==false){
+            tv_steps.setText("puuu");
+            if(podometre.isWakeUpSensor()==true){
+                tv_steps.setText("En vrai");
+            };
+        };
         running=true;
-        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if(countSensor !=null) {
-            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-            System.out.print("Ici");
-        }
-        else {
-            Toast.makeText(this, "pas trouvé", Toast.LENGTH_SHORT).show();
-        }
+     //   Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+      //  if(countSensor !=null) {
+        //    sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+          //  System.out.print("Ici");
+        //}
+        //else {
+          //  Toast.makeText(this, "pas trouvé", Toast.LENGTH_SHORT).show();
+        //}
 
+        super.onResume();
         }
 
     @Override
     protected void onPause() {
-        super.onPause();
-        running=false;
-        sensorManager.unregisterListener(this);
 
+        //running=false;
+        sensorManager.unregisterListener(this, podometre);
+        if(podometre.isWakeUpSensor()==false){
+            tv_steps.setText("peee");
+        };
+        super.onPause();
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-    if(running){
-        tv_steps.setText(String.valueOf(sensorEvent.values[0]));
+        float[] pas;
+        if(sensorEvent.sensor.getType()==Sensor.TYPE_STEP_COUNTER){
+            pas = sensorEvent.values;
+            tv_steps.setText("pas");
+
+        }
+        if(podometre.isWakeUpSensor()==false){
+            tv_steps.setText("changement");
+        };
     }
-    }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
