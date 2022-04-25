cfb5f376e4638914d716620665843c4f17c9c800
Hugo <hugo.lacombe@epfedu.fr>
2022-04-25 22:35:29 +0200

    1er co implémenter

--- app/src/main/java/com/example/applicationsiah/informations_perso.java
+++ app/src/main/java/com/example/applicationsiah/informations_perso.java
@@ -2,6 +2,11 @@ package com.example.applicationsiah;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class informations_perso extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class informations_perso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations_perso);
+        EditText editprenom;
+        editprenom = (EditText) findViewById(R.id.prenom);
+        Editable prenom;
+        prenom = editprenom.getText();
+        TextView text = (TextView) findViewById(R.id.textView45);
+
+        Button save = (Button)  findViewById(R.id.enregistrer);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                text.setText(prenom);
+            }
+        });
     }
 }
