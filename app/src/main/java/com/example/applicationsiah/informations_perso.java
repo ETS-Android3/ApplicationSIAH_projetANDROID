c0c27147c0ab2d6bdcbb6c1a0a378af1df75651f
Hugo <hugo.lacombe@epfedu.fr>
2022-04-25 20:21:36 +0200

    1er co implémenter

---
+++ app/src/main/java/com/example/applicationsiah/informations_perso.java
@@ -0,0 +1,13 @@
+package com.example.applicationsiah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class informations_perso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informations_perso);
+    }
+}
