6be3f7334d0ee6a7cc0de201c80d1d6c5637ee5f
Hugo <hugo.lacombe@epfedu.fr>
2022-05-07 00:24:28 +0200

    bouton retour fait dans la barre d'action !

--- app/src/main/java/com/example/applicationsiah/MainActivity.java
+++ app/src/main/java/com/example/applicationsiah/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.applicationsiah;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -7,22 +8,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
 Boolean co;
-private VideoView videoView;
+boolean retour=false;
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main,menu);
+
         return true;
       //  retu rn super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.retour){
+            retour=true;
+            Context context = getApplicationContext();
+            CharSequence text = "SIIUUUUUUU";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        }
+        return true;
     }
 
     @Override
