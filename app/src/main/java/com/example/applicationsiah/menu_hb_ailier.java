4a405639f41ad240be72bdd7c1ded6c74f43fdb4
ilonabematol <ilona.bematol@epfedu.fr>
2022-05-06 11:20:08 +0200

    hand ailier et arrière activités

--- app/src/main/java/com/example/applicationsiah/menu_hb_ailier.java
+++ app/src/main/java/com/example/applicationsiah/menu_hb_ailier.java
@@ -9,5 +9,6 @@ public class menu_hb_ailier extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_hb_ailier);
+
     }
 }
