66ccac120f3d1c515af6bb6c230f6b4bcbd02e5d
scottfullagar <scott.fullagar@epfedu.fr>
2022-05-06 11:22:56 +0200

    ajout activite a propos de nous

---
+++ app/src/main/java/com/example/applicationsiah/a_propos_de_nous.java
@@ -0,0 +1,13 @@
+package com.example.applicationsiah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class a_propos_de_nous extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apropos_de_nous);
+    }
+}
