bfa4fa633b3d6f6d28fc12d508bd8ca01ad8dd70
Axel <axel.fourcadecancelle@epfedu.fr>
2022-05-12 15:42:01 +0200

    physique foot fini

--- app/src/main/java/com/example/applicationsiah/menu_foot_gb.java
+++ app/src/main/java/com/example/applicationsiah/menu_foot_gb.java
@@ -3,11 +3,29 @@ package com.example.applicationsiah;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class menu_foot_gb extends AppCompatActivity {
+    boolean retour=false;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
 
+        return true;
+        //  retu rn super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.retour) {
+            retour = true;
+            retour();
+        }
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +41,9 @@ public class menu_foot_gb extends AppCompatActivity {
     private void visualiser_physique_gb() {
         Intent intent2 = new Intent(this, Physique_gb.class);
         startActivity(intent2);
+    }
+    private void retour() {
+        Intent intent60 = new Intent(this, menu_foot.class);
+        startActivity(intent60);
     }
 }
