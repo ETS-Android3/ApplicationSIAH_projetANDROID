d2d508ad13b7513bc939d58e729937967007e074
Axel <axel.fourcadecancelle@epfedu.fr>
2022-05-13 06:40:52 +0200

    hand quasi fini

--- app/src/main/java/com/example/applicationsiah/menu_hb_arriere.java
+++ app/src/main/java/com/example/applicationsiah/menu_hb_arriere.java
@@ -1,7 +1,10 @@
 package com.example.applicationsiah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menu_hb_arriere extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class menu_hb_arriere extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_hb_arriere);
+        Button button_tactique_gb = (Button) findViewById((R.id.button_tact_gb));
+        button_tactique_gb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                visualiser_la_tactiquegb();
+            }
+        });
+
+        Button button_exo_gb = (Button) findViewById((R.id.exo_gb));
+        button_exo_gb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                visualiser_la_exogb();
+            }
+        });
+    }
+
+    private void visualiser_la_exogb() {
+        Intent intent2 = new Intent(this, menu_hb_arriere_exo_spe.class);
+        startActivity(intent2);
+    }
+
+    private void visualiser_la_tactiquegb() {
+        Intent intent1 = new Intent(this, menu_hb_arriere_tactique.class);
+        startActivity(intent1);
     }
 }
